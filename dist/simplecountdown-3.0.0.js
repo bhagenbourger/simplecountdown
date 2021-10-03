5f4790b3206017640c058579f964c8c109f668d8
benoit <benoit@hagenbourger.fr>
2021-10-03 11:42:58 +0200

    Release 3.0.0

--- dist/simplecountdown-3.0.0.js
+++ dist/simplecountdown-3.0.0.js
@@ -54,7 +54,14 @@
    },
    loadCSS: function(theme){
     var cssDiv = document.createElement('div');
-    cssDiv.innerHTML = '<style>' + this.themes[theme].style.title + this.themes[theme].style.container + this.themes[theme].style.brick + this.themes[theme].style.number + this.themes[theme].style.legend + '</style>';
+    cssDiv.innerHTML = '<style>' +
+      this.themes[theme].style.title +
+      this.themes[theme].style.container +
+      this.themes[theme].style.brick +
+      this.themes[theme].style.number +
+      this.themes[theme].style.legend +
+      (this.themes[theme].style.custom || "") +
+      '</style>';
     document.getElementsByTagName('head')[0].appendChild(cssDiv.childNodes[0]);
    },
    display: function(container, deadline, theme, refresh){
@@ -65,8 +72,16 @@
     var t = this.computeTimeRemaining(deadline);
     if(!refresh){
     	document.getElementById(container).className += " sc-container";
+      document.getElementById(container).innerHTML =
+      '<div class="sc-title">' + this.themes[theme].content.title + '</div>' +
+      '<div id="sc-countdown"></div>' +
+      '<div class="sc-custom">' + (this.themes[theme].content.custom || "") + '</div>';
 	  }
-    document.getElementById(container).innerHTML = '<div class="sc-title">' + this.themes[theme].content.title + '</div><div class="sc-brick"><span class="sc-number">' + t.days + '</span><span class="sc-legend">Days</span></div><div class="sc-brick"><span class="sc-number">' + t.hours + '</span><span class="sc-legend">Hours</span></div><div class="sc-brick"><span class="sc-number">' + t.minutes + '</span><span class="sc-legend">Minutes</span></div><div class="sc-brick"><span class="sc-number">' + t.seconds + '</span><span class="sc-legend">Seconds</span></div>';
+    document.getElementById("sc-countdown").innerHTML =
+      '<div class="sc-brick"><span class="sc-number">' + t.days + '</span><span class="sc-legend">Days</span></div>' +
+      '<div class="sc-brick"><span class="sc-number">' + t.hours + '</span><span class="sc-legend">Hours</span></div>' +
+      '<div class="sc-brick"><span class="sc-number">' + t.minutes + '</span><span class="sc-legend">Minutes</span></div>' +
+      '<div class="sc-brick"><span class="sc-number">' + t.seconds + '</span><span class="sc-legend">Seconds</span></div>';
     if(!refresh && this.callback){
       this.callback();
     }
